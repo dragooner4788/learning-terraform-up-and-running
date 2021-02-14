5b8aa342cdeca499829d20cddbdf0065a2a0ff29
Guy Friley <gfriley@hotmail.com>
2021-02-14 01:17:39 -0500

    changed aws_instance to aws_launch_configuration

--- ch2-deploy-single-server/main.tf
+++ ch2-deploy-single-server/main.tf
@@ -2,62 +2,36 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF   
-
-  tags = {
-    "Name" = "terraform-example"
-  }
 }
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-ingress = [ {
-  cidr_blocks = [ "0.0.0.0/0" ]
-  description = "Allows for communication with Web Server to say Hello World."
-  from_port = var.server_port
-  ipv6_cidr_blocks = null
-  prefix_list_ids = null
-  protocol = "tcp"
-  security_groups = null
-  self = null
-  to_port = var.server_port
-} ]
-} 
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "Allows for communication with Web Server to say Hello World."
+    from_port        = var.server_port
+    ipv6_cidr_blocks = null
+    prefix_list_ids  = null
+    protocol         = "tcp"
+    security_groups  = null
+    self             = null
+    to_port          = var.server_port
+  }]
+}
 
 variable "server_port" {
   description = "Port used for HTTP requests."
-  type = number
-  default = 8080
-}
-
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-  description = "Gives public IP address of instance."
-  sensitive = false
-}
-
-output "instance_public_dns" {
-  value = aws_instance.example.public_dns
-  description = "Shows name of instance."
-  sensitive = false
-}
-
-output "instance_arn"{
-  value = aws_instance.example.arn
-  description = "Shows arn of instance."
-  sensitive = false
-}
-
-output "instance_name" {
-  value = aws_instance.example.tags.Name
+  type        = number
+  default     = 8080
 }
