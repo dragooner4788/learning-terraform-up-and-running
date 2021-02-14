30082c8e5f6309b681eecba40fdb61ee7710576b
Guy Friley <gfriley@hotmail.com>
2021-02-13 21:14:51 -0500

    cleaned up SG

--- ch2-deploy-single-server/main.tf
+++ ch2-deploy-single-server/main.tf
@@ -16,3 +16,15 @@ resource "aws_instance" "example" {
     "Name" = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    description = "allow all ip's to access resource"
+  } ]
+}
